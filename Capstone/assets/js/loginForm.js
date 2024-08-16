df4a71d5cc0d0670fc42770f379165a175ca773e
FirstName Falcatan <kmfalcatan2@gmail.com>
2024-08-16 16:14:56 +0800

    aksjbdaks

--- Capstone/assets/js/loginForm.js
+++ Capstone/assets/js/loginForm.js
@@ -8,6 +8,16 @@ function login(){
     }
 }
 
+function login1(){
+    const loginButton = document.querySelector(' .errorContainer');
+
+    if(loginButton.style.display === 'none'){
+        loginButton.style.display = 'block';
+    } else{
+        loginButton .style.display = 'none'
+    }
+}
+
 function error(){
     const errorButton = document.querySelector(' .errorContainer');
 
